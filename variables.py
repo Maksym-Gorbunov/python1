2c95f9038311c81e8084500aec2352e3628d8187
Maksym-Gorbunov <maxsverige@hotmail.com>
2018-04-03 23:30:24 +0100

    sOME CHANGES

---
+++ variables.py
@@ -0,0 +1,13 @@
+myStr = 'Maksym'
+myInt = 32
+myFloat = 32.3
+myList = [1,2,3,'list']
+myDict = {'a': 1, 
+		  'b': 2, 
+		  'c': 3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
